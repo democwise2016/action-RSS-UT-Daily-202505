5eaf7d025ba600ebff1bf41336e83ec1366b5940
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-05-11 02:57:13 +0800

    西元2025年05月11日 (週日) 02時57分13秒 CST

---
+++ config-list.js
@@ -0,0 +1,57 @@
+const ItemFiltersPreset = require('./app/config/ItemFiltersPreset.js')
+const CONFIG = require('../config-json.js')
+
+let feedList = [
+
+  {
+    title: '志祺七七 X 圖文不符',
+    feedID: 'shasha77',
+    homepageURL: 'https://www.youtube.com/channel/UCiWXd0nmBjlKROwzMyPV-Nw',
+    itemFilters: [
+      ItemFiltersPreset.between3minTo30Min,
+      (item) => { return (item.title.endsWith('｜志祺七七')) }
+    ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+  },
+  {
+    title: 'Jer仔',
+    feedID: 'jer965',
+    homepageURL: 'https://www.youtube.com/channel/UClkSKv9-Geah2gwYu7e9jFw',
+    itemFilters: [
+      ItemFiltersPreset.between3minTo30Min,
+    ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+  },
+  {
+    title: '攝徒日記Fun TV',
+    feedID: 'funtv8964',
+    homepageURL: 'https://www.youtube.com/channel/UCvTe3Z7TZsjGzUERx4Ce6zA',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
+  {
+    title: '公子沈',
+    feedID: 'gongzishen',
+    homepageURL: 'https://www.youtube.com/channel/UCrGSFNEBmCN0rqhATZels2Q',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
+]
+
+// 
+
+// --------------------------------------
+
+if (CONFIG.debug) {
+  feedList = [
+    {
+      title: '志祺七七 X 圖文不符',
+      feedID: 'shasha77',
+      homepageURL: 'https://www.youtube.com/channel/UCiWXd0nmBjlKROwzMyPV-Nw',
+      itemFilters: [
+        ItemFiltersPreset.between3minTo30Min,
+        (item) => { return (item.title.endsWith('｜志祺七七')) }
+      ], // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+      maxItems: 3
+    },
+  ]
+}
+
+
+module.exports = feedList
